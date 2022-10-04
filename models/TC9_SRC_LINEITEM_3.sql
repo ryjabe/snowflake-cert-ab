af7d9b299b9fc2a2a6c8bceeab1dffe96df6f890
Ryan <ryan@airbyte.io>
2022-10-04 16:00:21 -0500

    Add files via upload

---
+++ models/TC9_SRC_LINEITEM_3.sql
@@ -0,0 +1,33 @@
+{{ config(
+    cluster_by = ["_AIRBYTE_EMITTED_AT"],
+    unique_key = '_AIRBYTE_AB_ID',
+    schema = "DBT_TEMP",
+    tags = [ "top-level" ]
+) }}
+-- Final base SQL model
+-- depends_on: {{ ref('TC9_SRC_LINEITEM_3_AB3') }}
+select
+    L_TAX,
+    L_COMMENT,
+    L_PARTKEY,
+    L_SUPPKEY,
+    L_DISCOUNT,
+    L_ORDERKEY,
+    L_QUANTITY,
+    L_SHIPDATE,
+    L_SHIPMODE,
+    L_COMMITDATE,
+    L_LINENUMBER,
+    L_LINESTATUS,
+    L_RETURNFLAG,
+    L_RECEIPTDATE,
+    L_SHIPINSTRUCT,
+    L_EXTENDEDPRICE,
+    _AIRBYTE_AB_ID,
+    _AIRBYTE_EMITTED_AT,
+    {{ current_timestamp() }} as _AIRBYTE_NORMALIZED_AT,
+    _AIRBYTE_TC9_SRC_LINEITEM_3_HASHID
+from {{ ref('TC9_SRC_LINEITEM_3_AB3') }}
+-- TC9_SRC_LINEITEM_3 from {{ source('DBT_TEMP', '_AIRBYTE_RAW_TC9_SRC_LINEITEM_3') }}
+where 1 = 1
+
